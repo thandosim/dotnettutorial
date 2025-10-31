4ed2bc180a250e7ff64d26a2bb685b94691fd025
thandosim <sim24001@byui.edu>
2025-10-31 22:33:37 +0200

    Initial commit for .Net turorial

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("The current time is " + DateTime.Now);
+DateTime christmas = new DateTime(DateTime.Now.Year, 12, 25);
+Console.WriteLine("There are " + (christmas - DateTime.Now).Days + " days until Christmas!");
